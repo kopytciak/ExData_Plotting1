4c3b9cae95a6443fe9070be58f01475d3667a823
kopytciak <kopyt_ko@yahoo.de>
2015-06-08 00:34:41 +0100

    4th plot

--- Plot4.R
+++ Plot4.R
@@ -14,8 +14,21 @@ rawFrame$DateTime <- paste(rawFrame$Date, rawFrame$Time)
 rawFrame$DateTime <- strptime(rawFrame$DateTime, format = '%d/%m/%Y %H:%M:%S')
 
 #prepare graphic device 
-png(filename='plot3.png', width = 480, height = 480, units = 'px', bg = 'white')
+png(filename='plot4.png', width = 480, height = 480, units = 'px', bg = 'white')
 
+#Graphic with 2 lnes and two columns
+par(mfrow=c(2,2))
+
+
+# Graphic 1 Plot
+plot(rawFrame$DateTime, rawFrame$Global_active_power,
+     type="l",
+     xlab="",
+     ylab="Global Active Power")
+
+# Graphic 2 Plot
+plot(rawFrame$DateTime, rawFrame$Voltage, type="l",
+     xlab="datetime", ylab="Voltage")
 
 # Graphic Plot
 plot(rawFrame$DateTime, rawFrame$Sub_metering_1, col = 'black',
@@ -27,6 +40,13 @@ lines(rawFrame$DateTime, rawFrame$Sub_metering_2, col='red')
 lines(rawFrame$DateTime, rawFrame$Sub_metering_3, col='blue')
 legend("topright",col=c("black", "red", "blue"), c("Sub_metering_1", 
                                                    "Sub_metering_2", "Sub_metering_3"), lty=1)
+
+
+
+# Graphic 4 Plot
+plot(rawFrame$DateTime, rawFrame$Global_reactive_power, type="n",
+     xlab="datetime", ylab="Global_reactive_power")
+lines(rawFrame$DateTime, rawFrame$Global_reactive_power)
 
 #clean up graphic device
 dev.off()
